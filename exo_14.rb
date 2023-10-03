1890feb37ef47ac992a25f88558e26ddba042d49
LouisKzy <louiskrejezy@gmail.com>
2023-10-03 14:21:39 +0200

    final commit

--- exo_14.rb
+++ exo_14.rb
@@ -1,11 +1,11 @@
 nb_mails = 1 
 print "Combien d'email voulez vous generez ? >"
-range = gets.chomp.to_i
+range = gets.chomp.to_i # stock le nb de foi qu'on va itéré la boucle
 print "Quel est votre prenom ? >"
-prenom = gets.chomp
+prenom = gets.chomp  # option personalisation 
 print "quel est votre nom ? >"
-nom = gets.chomp
-data_base = []
+nom = gets.chomp # option personalisation 
+data_base = []  # création d'un tableau
 
 while nb_mails <= range # generation pour nb email
   email = "#{prenom}.#{nom}#{nb_mails}@email.com" # comment l'email est formé
@@ -13,9 +13,9 @@ while nb_mails <= range # generation pour nb email
   nb_mails += 1 
 end
 
-data_base.each do |email|
-  numero_email = email[-6]
-  if numero_email.even?
+data_base.each do |email| # pour chaque élement stocké dans data base 
+  numero_email = email[-11].to_i  # ressort le 11eme caractère en partant de la fin en tant que integer 
+  if numero_email.even? #  compare si numéro de l'empail == pair
     puts email
   end
 end
