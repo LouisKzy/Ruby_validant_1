abf884a370abba3a3670adbfe4fa19b98fd0efc1
LouisKzy <louiskrejezy@gmail.com>
2023-10-03 17:20:27 +0200

    travail boucle condition ( exemple unless exo 16 a travaillez )

--- exo_15.rb
+++ exo_15.rb
@@ -1,5 +1,10 @@
 print " Hello world ! Bienvenue dnas la super pyramide ! Combien d'étages veux tu ? > "
 floor = gets.chomp.to_i
+
+while floor >= 25 or floor <=1
+  print " j'ai dit un nombre entre 1 et 25"
+  floor = gets.chomp.to_i
+end
 debut_etage = 1
 while debut_etage <= floor
   puts "#" * debut_etage
--- exo_16.rb
+++ exo_16.rb
@@ -1,9 +1,14 @@
-print " Hello world ! Bienvenue dnas la super pyramide ! Combien d'étages veux tu ? > "
+print " Hello world ! Bienvenue dans la super pyramide ! Combien d'étages veux tu ? > "
 floor = gets.chomp.to_i
 debut_etage = 1
+unless (1..25).to_a.include?(floor) #doit boucler tant que floor n'est pas entre 1 et 25
+  print " j'ai dit un nombre entre 1 et 25 >"
+  floor = gets.chomp.to_i
+end
+
 while debut_etage <= floor
   espaces = " " * (floor - debut_etage) # gère les espaces au début 
-  carct = "#" * debut_etage # gère les "#"" affiché
+  carct = "#" * debut_etage # gère les "#" affiché
   puts espaces + carct  # affiche la pyramide à "l'envers"
   debut_etage += 1 # incrémente les étage de 1
 end
